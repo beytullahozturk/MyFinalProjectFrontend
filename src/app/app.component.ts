3626593a9df337cba0c45f20325c35f90623b222
Beytullah Öztürk <ozbeytullah2@gmail.com>
2021-03-11 09:47:25 +0300

    11.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // {} : obje demektir
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'northwind';
+  user: string = 'Beytullah';
+  // any = her veri türü olabilir demek ve yazmasak da olur
+  
 }
